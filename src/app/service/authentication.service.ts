9a632efb8b1fbcc54db49fefa764477e107db422
Nan Andrei David <andrei.nan03@e-uvt.ro>
2023-05-17 20:30:57 +0300

    Implemented user 'register', 'login' services and started implementing the 'logOut' service

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpResponse } from  '@angular/common/http'
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private host = environment.apiUrl
+  private token: null | undefined;
+  private loggedInUsername: null | undefined;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    // we set the observe be 'response' to aslo return the headers, not just the body
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, {observe: 'response'})
+
+  }
+
+  public register(user: User): Observable<User | HttpErrorResponse> {
+    // here we only need the response body from the observable
+    return this.http.post<User | HttpErrorResponse> (`${this.host}/user/register`, user);
+  }
+
+  public logOut(): void {
+    this.token = null;
+    this.loggedInUsername = null;
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('users');
+
+  }
+
+
 }
